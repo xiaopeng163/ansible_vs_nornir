182239d32cdf5496a0c628a569c59b19ed517449
Peng Xiao <xiaoquwl@gmail.com>
2020-10-29 22:18:11 +0100

    vagrant

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,4 +1,10 @@
 #/bin/sh
 
 # install some tools
-sudo apt-get install -y git vim gcc build-essential
+sudo apt-get install -y git vim gcc build-essential python3-venv
+git clone https://github.com/xiaopeng163/ansible_vs_nornir
+cd ansible_vs_nornir
+python3 -m venv venv
+source venv/bin/activate
+pip install wheel
+pip install -r requirements.txt
